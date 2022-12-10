055111e6132e7b8c3a18a50a8522208301e34e85
yunsung <yunsung@marvrus.com>
2022-12-11 00:15:46 +0900

    fix/useProduct : 상품의 score를 기준으로 높은 점수부터 낮은 점수로 내림차순하도록 option 추가

--- src/hooks/useProducts.ts
+++ src/hooks/useProducts.ts
@@ -1,10 +1,22 @@
 import { useQuery } from '@tanstack/react-query';
+import { a, b } from 'msw/lib/SetupApi-b2f0e5ac';
 import { productApi } from 'src/api/productApi';
 
 const useProducts = () => {
-  return useQuery(['products'], () => {
-    return productApi.getProducts();
-  });
+  return useQuery(
+    ['products'],
+    () => {
+      return productApi.getProducts();
+    },
+    {
+      select: (data) => {
+        const sortingData = data.sort((a: any, b: any) => {
+          return b.score - a.score;
+        });
+        return sortingData;
+      },
+    },
+  );
 };
 
 export { useProducts };
