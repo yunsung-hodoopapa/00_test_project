d7cf0453c65afc268cdf7a668f6f629cf3b19e73
yunsung <yunsung@marvrus.com>
2022-12-11 00:42:51 +0900

    feat/productCard: ImageWrapper 추가 및 nextConfig Image 관련 룰 추가

--- src/components/productCard/index.tsx
+++ src/components/productCard/index.tsx
@@ -1,13 +1,22 @@
+/* eslint-disable no-unused-vars */
 import styled from '@emotion/styled';
+import Image from 'next/image';
 
 const StyledCard = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: start;
-  margin: auto;
+  width: 250px;
+  margin: 10px 10px;
   transition: all 250ms ease-in-out;
-  border: 1px solid blue;
+  border: 1px solid grey;
   border-radius: 8px;
+`;
+
+const ProductDescriptionWrap = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
 `;
 
 const ProductCard = (props: any) => {
@@ -18,7 +27,11 @@ const ProductCard = (props: any) => {
 
   return (
     <StyledCard>
-      <span>{item_name}</span>
+      <Image src={detail_image_url} width={250} height={250} alt="상품이미지" />
+      <ProductDescriptionWrap>
+        <span>{item_name}</span>
+        <span>{price}</span>
+      </ProductDescriptionWrap>
     </StyledCard>
   );
 };
