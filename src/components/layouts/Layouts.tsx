8eb9b3871f5feb325303ca14fa06d1f2613616b1
yunsung <yunsung@marvrus.com>
2022-12-11 21:37:53 +0900

    feat/Layout: 홈 레이아웃 추가 및 헤더 내 카트아이콘 추가, 카트 아이콘 클릭 시 /cart로 이동

---
+++ src/components/layouts/Layouts.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Head from 'next/head';
+import Header from './Header';
+import Footer from './Footer';
+
+type Props = {
+  children: React.ReactNode;
+};
+
+const Layouts: React.FC<Props> = ({ children }) => {
+  return (
+    <>
+      <Head>
+        <title>29cm 과제 프로젝트</title>
+      </Head>
+      <Header />
+      <div>{children}</div>
+      <Footer />
+    </>
+  );
+};
+
+export default Layouts;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,8 +3,14 @@ import { dehydrate, QueryClient } from '@tanstack/react-query';
 import { productApi } from 'src/api/productApi';
 import ProductList from 'src/components/productList';
 
+import Layouts from 'src/components/layouts/Layouts';
+
 const Home: NextPage = () => {
-  return <ProductList />;
+  return (
+    <Layouts>
+      <ProductList />
+    </Layouts>
+  );
 };
 
 export async function getStaticProps() {
