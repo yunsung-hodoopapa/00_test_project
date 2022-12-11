7885aab630a57f00eef5aa86dc0deb8c0f02c08b
yunsung <yunsung@marvrus.com>
2022-12-11 21:37:14 +0900

    chore/svg 파일을 인식할 수 있도록 next.config 설정 추가 및 의존성 설치

--- next.config.js
+++ next.config.js
@@ -4,6 +4,14 @@ const nextConfig = {
   images: {
     domains: ['localhost', 'img.29cm.co.kr'],
   },
+  webpack: (config) => {
+    config.resolve.modules.push(__dirname); // 추가
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+    return config;
+  },
 };
 
 module.exports = nextConfig;
