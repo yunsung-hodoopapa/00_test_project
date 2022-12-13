3027e3dee401b8c71b335324376f6a60378a3b16
yunsung <yunsung@marvrus.com>
2022-12-13 19:12:47 +0900

    chore: 글로벌 css 적용 및 컬러 테마 설정

---
+++ src/styles/emotion.d.ts
@@ -0,0 +1,13 @@
+import '@emotion/react';
+
+export type themeId = 'grey' | 'primary' | 'secondary' | 'success' | 'error';
+
+declare module '@emotion/react' {
+  export interface Theme {
+    [key in themeId]: {
+      background: string;
+      color: string;
+      hoverBackground: string;
+    };
+  }
+}
---
+++ src/styles/global.ts
@@ -0,0 +1,30 @@
+import { css } from '@emotion/react';
+
+const global = css`
+  html,
+  body {
+    padding: 0px;
+    margin: 0px;
+    font-size: 62.5%;
+    font-family: sans-serif;
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  * {
+    box-sizing: border-box;
+  }
+
+  span {
+    font-size: 1.4rem;
+  }
+
+  h1 {
+    font-size: 2.5rem;
+    margin: 0px;
+  }
+`;
+
+export default global;
