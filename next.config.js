d51628b0881ae24b202bdaff148d91b0a32a8493
yunsung <yunsung@marvrus.com>
2022-12-13 10:16:02 +0900

    fix: 홈 라우트 진입 시 products로 이동할 수 있도록 config룰 추가, '/'페이지 컴포넌트 정리

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/products',
+        permanent: false,
+      },
+    ];
+  },
   images: {
     domains: ['localhost', 'img.29cm.co.kr'],
   },
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,30 +1,12 @@
 /* eslint-disable no-unused-vars */
 import type { NextPage } from 'next';
-import { dehydrate, QueryClient } from '@tanstack/react-query';
-import { productApi } from 'src/api/productApi';
-import ProductList from 'src/components/productList';
-import Layouts from 'src/components/layouts/Layouts';
 
 const Home: NextPage = () => {
   return (
-    <Layouts>
-      <ProductList />
-    </Layouts>
+    <>
+      <div>home</div>
+    </>
   );
 };
-
-export async function getStaticProps() {
-  const queryClient = new QueryClient();
-
-  await queryClient.prefetchQuery(['products', 0], () => {
-    return productApi.getProducts(0);
-  });
-
-  return {
-    props: {
-      dehydratedState: dehydrate(queryClient),
-    },
-  };
-}
 
 export default Home;
