13b82ab59250c235d5754216a649d9712fcf06af
yunsung <yunsung@marvrus.com>
2022-12-26 17:19:34 +0900

    fix/UseCart : 쿼리로 불러온 데이터를 얉은 복사 후 조정하도록 수정

--- src/hooks/useProducts.ts
+++ src/hooks/useProducts.ts
@@ -7,7 +7,8 @@ const useProducts = (page: number) =>
     keepPreviousData: false,
     // TODO: useQuery에서 select문으로 가져온 데이터를 shallow 카피하지 않고, 가공하여 cached된 데이터가 수정된다. 이 부분 수정하기
     select: (data) => {
-      const sortingData = data.contents.sort(
+      const shallowdata = [...data.contents]; // 쿼리를 통해 반환된 데이터는 반드시 안정적이어야 한다. 불변성을 지키자! https://github.com/TanStack/query/discussions/1304
+      const sortingData = shallowdata.sort(
         (a: ProductInfoType, b: ProductInfoType) => b.score - a.score,
       );
       const hasMore = data.totalPages > page;
